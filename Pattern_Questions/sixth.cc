dc08672bf1a62426fd10c40fd98a2cb6b3a6e1f3
ashish-205 <85436925+ashish-205@users.noreply.github.com>
2022-09-07 10:29:22 +0530

    Create sixth.cc

---
+++ Pattern_Questions/sixth.cc
@@ -0,0 +1,25 @@
+/*
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row;
+    cin>>row;
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<=row-i-1;j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
