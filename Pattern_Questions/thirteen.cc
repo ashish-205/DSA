a09503774afdc64748109574825c0f66848c64bc
ashish-205 <85436925+ashish-205@users.noreply.github.com>
2022-09-07 22:54:14 +0530

    Create thirteen.cc

---
+++ Pattern_Questions/thirteen.cc
@@ -0,0 +1,33 @@
+/*
+* * * * 
+*     * 
+*     * 
+* * * * 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout<<"Enter rows :";
+    cin>>row;
+
+    
+ for(int i=0;i<row;i++)
+ {
+    for(int j=0;j<row;j++)
+    {
+        if(i==0 || i==row-1 ||j==0 || j== row-1)
+        {
+            cout<<"* ";
+        }
+        else{
+            cout<<"  ";
+        }
+    }
+    cout<<endl;
+ }
+
+    
+}
