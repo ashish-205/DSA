3daa64823bb8971a65f803aa204c8680048ffd7d
ashish-205 <85436925+ashish-205@users.noreply.github.com>
2022-09-14 23:02:49 +0530

    Create seventeen.cc

---
+++ Pattern_Questions/seventeen.cc
@@ -0,0 +1,49 @@
+/*
+      *
+     ***
+    *****
+   *******
+    *****
+     ***
+      *
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int row;
+    cout<<"Enter rows :";
+    cin>>row;
+
+    for(int i=0;i<row-1;i++)
+ {
+    for(int k=0;k<row-i-1;k++)
+    {
+        cout<<" ";
+    }
+    for(int j=0;j<2*i+1;j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+ }
+ for(int i=0;i<row;i++)
+ {
+    
+    for(int k=0;k<i;k++)
+    {
+        cout<<" ";
+    }
+    for(int j=0;j<2*(row-i-1)+1;j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+ }
+ 
+
+    
+}
